c6a5692b1648d251fc255d8366038c81432a8e65
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-01-26 21:22:31 +0400

    Constants

---
+++ src/myEngGame/Constants.java
@@ -0,0 +1,26 @@
+package myEngGame;
+
+public enum Constants {
+    START("if you are admin add number [1]\n" +
+            "if you are user add any number other than [1]"),
+    STEP1("1. get all file\n" +
+            "2. add file\n" +
+            "3. update file\n" +
+            "4. delete file\n" +
+            "5. back"),
+    PLAY("1. Aze :\n" +
+            "2. Eng :\n" +
+            "4. Back to Main Menu"),
+    DONE_OR_STOP("1. Done\n" +
+            "2. Stop\n"),
+    WRONG_INPUT("pls add correct input") , ADD_AZE_WORD("add azeWord :") , ADD_ENG_WORD("add engWord :") ,
+    WORD_UPDATE("Word Updated") , WORD_ADDED("Word Added") , WORD_DELETED("Word Deleted") ,
+    ADD_WORDS_ID("Add Words Id :") , ADD_NEW_AZE_WORD("Add new azeWord :") , ADD_NEW_ENG_WORD("Add new engWord :");
+    private String s;
+    Constants(String s) {
+        this.s=s;
+    }
+    public String getS() {
+        return s;
+    }
+}
